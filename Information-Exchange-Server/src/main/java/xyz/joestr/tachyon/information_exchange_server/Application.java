43b7848ab07cc9056e7efc5d8ab54bc9b8bbc6d7
Joel Strasser <strasser999@gmail.com>
2019-10-14 22:01:11 +0200

    [TODO] configuration files;

--- Information-Exchange-Server/src/main/java/xyz/joestr/tachyon/information_exchange_server/Application.java
+++ Information-Exchange-Server/src/main/java/xyz/joestr/tachyon/information_exchange_server/Application.java
@@ -1,12 +1,25 @@
 package xyz.joestr.tachyon.information_exchange_server;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.yaml.snakeyaml.Yaml;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    private File localCOnfigurationFile;
+    private File externalConfigurationFile;
+    private Yaml configuration;
+    
+    public void main(String[] args) throws URISyntaxException {
+        
+        this.localCOnfigurationFile  = new File(this.getClass().getResource("config.yml").toURI());
+        this.externalConfigurationFile = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath(), "config.yml");
+        
+        
+        
         SpringApplication.run(Application.class, args);
     }
 }
