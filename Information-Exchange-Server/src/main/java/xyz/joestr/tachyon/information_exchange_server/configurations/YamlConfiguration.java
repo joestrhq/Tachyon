dabad8bdace31fa597c6618d11786fdc2b45396e
Joel Strasser <strasser999@gmail.com>
2019-10-24 23:07:24 +0200

    updated documentation of yamlconfiguration;

--- Information-Exchange-Server/src/main/java/xyz/joestr/tachyon/information_exchange_server/configurations/YamlConfiguration.java
+++ Information-Exchange-Server/src/main/java/xyz/joestr/tachyon/information_exchange_server/configurations/YamlConfiguration.java
@@ -8,7 +8,28 @@ package xyz.joestr.tachyon.information_exchange_server.configurations;
 import java.util.List;
 
 /**
- *
+ * Represents the configuration saved in a YAML file.<br>
+ * <br>
+ * The file contains <b>must</b> contain following entries:
+ * <ul>
+ * <li>The <b>listen address</b></li>
+ * <li>The <b>listen Port</b></li>
+ * <li>The <b>url</b> for the BungeeCord instance</li>
+ * <li>The <b>urls</b> for the Bukkit instance</li>
+ * </ul>
+ * 
+ * The listen address <b>must</b> be provided in following format:
+ * {@code address: "127.0.0.1"}<br>
+ * <br>
+ * The listen port <b>must</b> be provided in following format:
+ * {@code port: 8080}<br>
+ * <br>
+ * The URL for the BungeeCord instance <b>must</b> be provided in following
+ * format: {@code bungeecordinstanceurl: "http://127.0.0.1:59801"}<br>
+ * <br>
+ * The URLs for the Bukkit instance <b>must</b> be provided in following format:
+ * {@code bukkitinstanceurls: ["http://127.0.0.1:59802", "http://127.0.0.1:59803"]}
+ * 
  * @author Joel
  */
 public class YamlConfiguration {
